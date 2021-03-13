77ace96f99612c820513abae6a898fd2135b0dd2
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-03-13 12:53:29 -0600

    colors, header and asset management setup

---
+++ src/assets/icons.js
@@ -0,0 +1,44 @@
+import arrowIconDark from "./icons/arrowIconDark.svg"
+import backIconDark from "./icons/backIconDark.svg"
+import closeIconDark from "./icons/closeIconDark.svg"
+import directionsIconDark from "./icons/directionsIconDark.svg"
+import pinIconDark from "./icons/pinIconDark.svg"
+import sendIconDark from "./icons/sendIconDark.svg"
+import timeIconDark from "./icons/timeIconDark.svg"
+import twitterIconDark from "./icons/twitterIconDark.svg"
+import searchIconDark from "./icons/searchIconDark.svg"
+
+import arrowIconLight from "./icons/arrowIconLight.svg"
+import backIconLight from "./icons/backIconLight.svg"
+import closeIconLight from "./icons/closeIconLight.svg"
+import directionsIconLight from "./icons/directionsIconLight.svg"
+import pinIconLight from "./icons/pinIconLight.svg"
+import sendIconLight from "./icons/sendIconLight.svg"
+import timeIconLight from "./icons/timeIconLight.svg"
+import twitterIconLight from "./icons/twitterIconLight.svg"
+import searchIconLight from "./icons/searchIconLight.svg"
+
+
+export const iconsDark = {
+    arrowIcon: arrowIconDark,
+    backIcon: backIconDark,
+    closeIcon: closeIconDark,
+    directionsIcon: directionsIconDark,
+    pinIcon: pinIconDark,
+    searchIcon: searchIconDark,
+    sendIcon: sendIconDark,
+    timeIcon: timeIconDark,
+    twitterIcon: twitterIconDark,
+}
+
+export const iconsLight = {
+    arrowIcon: arrowIconLight,
+    backIcon: backIconLight,
+    closeIcon: closeIconLight,
+    directionsIcon: directionsIconLight,
+    pinIcon: pinIconLight,
+    searchIcon: searchIconLight,
+    sendIcon: sendIconLight,
+    timeIcon: timeIconLight,
+    twitterIcon: twitterIconLight,
+}
