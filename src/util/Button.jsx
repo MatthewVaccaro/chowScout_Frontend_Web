5e5d9f06b5581c19d78764e8b86ccbbde3c42bc4
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-03-14 18:06:21 -0500

    hero header done

--- src/util/Button.jsx
+++ src/util/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Button({content, icon, color="white", background}) {
     return ( 
-        <button className={`bg-${background} flex items-center py-4 px-6 rounded-md`} >
-            <p className={`text-${color} font-medium mr-4`} > {content} </p>
+        <button className={`bg-${background} flex items-center py-4 px-6 rounded-md hover:bg-${background}50`} >
+            { content ? <p className={`text-${color} font-medium mr-4`} > {content} </p> : ""}
             <img src={icon} alt="icon"/>
         </button>    
     )
