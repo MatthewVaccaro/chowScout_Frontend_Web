55e54c6cd298719c606c534bdd7a1a504d3a1978
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-04-01 23:04:32 -0500

    google analytics is now tracking

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './styles/main.css';
 import {Route} from "react-router-dom";
 import LandingPage from "./views/landingPage/landingPage"
 import Input from './utility/components/Input';
+import GoogleAnalytics from "react-ga";
 
 function App() {
+  GoogleAnalytics.initialize("UA-193650779-1");
+
+  const tracker = ({ location }) => {
+    GoogleAnalytics.set({ page: location.pathname });
+    GoogleAnalytics.pageview(location.pathname);
+    return null;
+  };
+
+  console.log(tracker)
+
   return (
   <div className="mainContainer sm:mx-auto px-4 md:px-12 lg:px-16" >
+     <Route render={tracker} />
      <Route exact path="/" component={LandingPage} />
      <Route exact path="/testing" component={Input} />
   </div>
