fd2aefc1744c3068204615888078ee9459e53e10
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-05-25 09:01:24 -0500

    mCreated molecule dish completed

--- src/ui/primitives/atoms/dishContents.jsx
+++ src/ui/primitives/atoms/dishContents.jsx
@@ -3,7 +3,7 @@ function DishContents({ title, price, description, divider = false }) {
 		<div>
 			<div className="flex justify-between mb-1 ">
 				<p className="font-semibold"> {title} </p>
-				<p> {price ? "$" + price : "--"} </p>
+				<p className="font-medium"> {price ? "$" + price : "--"} </p>
 			</div>
 			<h5 className="text-black70"> {description} </h5>
 			{divider ? <div className="w-full h-px bg-black my-3 opacity-10 " /> : ""}
--- src/ui/primitives/molecules/dish.jsx
+++ src/ui/primitives/molecules/dish.jsx
@@ -1,13 +1,11 @@
 import DishContents from "../atoms/dishContents";
 import CardContainer from "../atoms/cardContainer";
 
-function Dish() {
+function Dish({ title, price, description, divider, extraStyles }) {
 	return (
 		<div>
-			<CardContainer extraStyles="">
-				<DishContents title="My Title" price="99" description="this is my description" divider={true} />
-				<DishContents title="My Title" price="99" description="this is my description" divider={true} />
-				<DishContents title="My Title" price="99" description="this is my description" />
+			<CardContainer extraStyles={extraStyles}>
+				<DishContents title={title} price={price} description={description} divider={divider} />
 			</CardContainer>
 		</div>
 	);
