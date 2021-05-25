372d58753c2e523ca0543ecf0e49d2d86ab5b995
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-05-25 09:07:14 -0500

    updated naming from extraStyles to containerStyles for improved naming convetion and legability

--- src/ui/primitives/molecules/dish.jsx
+++ src/ui/primitives/molecules/dish.jsx
@@ -1,10 +1,10 @@
 import DishContents from "../atoms/dishContents";
 import CardContainer from "../atoms/cardContainer";
 
-function Dish({ title, price, description, divider, extraStyles }) {
+function Dish({ title, price, description, divider, containerStyles }) {
 	return (
 		<div>
-			<CardContainer extraStyles={extraStyles}>
+			<CardContainer extraStyles={containerStyles}>
 				<DishContents title={title} price={price} description={description} divider={divider} />
 			</CardContainer>
 		</div>
