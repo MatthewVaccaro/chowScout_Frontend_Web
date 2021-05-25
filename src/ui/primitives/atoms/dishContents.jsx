c28ab904a559985639c1a6ceba34005714e6525c
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-05-25 08:51:19 -0500

    shard atom Card Container completed

--- src/ui/primitives/atoms/dishContents.jsx
+++ src/ui/primitives/atoms/dishContents.jsx
@@ -1,12 +1,12 @@
 function DishContents({ title, price, description, divider = false }) {
 	return (
 		<div>
-			<div className="flex justify-between mb-2 ">
+			<div className="flex justify-between mb-1 ">
 				<p className="font-semibold"> {title} </p>
 				<p> {price ? "$" + price : "--"} </p>
 			</div>
 			<h5 className="text-black70"> {description} </h5>
-			{divider ? <div className="w-full h-px bg-black20 my-3" /> : ""}
+			{divider ? <div className="w-full h-px bg-black my-3 opacity-10 " /> : ""}
 		</div>
 	);
 }
--- src/ui/primitives/molecules/dish.jsx
+++ src/ui/primitives/molecules/dish.jsx
@@ -1,7 +1,16 @@
 import DishContents from "../atoms/dishContents";
+import CardContainer from "../atoms/cardContainer";
 
 function Dish() {
-	return <div></div>;
+	return (
+		<div>
+			<CardContainer extraStyles="">
+				<DishContents title="My Title" price="99" description="this is my description" divider={true} />
+				<DishContents title="My Title" price="99" description="this is my description" divider={true} />
+				<DishContents title="My Title" price="99" description="this is my description" />
+			</CardContainer>
+		</div>
+	);
 }
 
 export default Dish;
