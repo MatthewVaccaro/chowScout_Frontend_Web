b5847e7dc98d3825916bd91b3210bbe95d1d6843
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-07-10 16:49:22 -0500

    button presets addded. Some buttons might be effected

--- src/ui/primitives/Button.jsx
+++ src/ui/primitives/Button.jsx
@@ -1,13 +1,51 @@
 import React from "react";
+import { iconsDark } from "../assets/icons";
+import { useHistory } from "react-router-dom";
 
-function Button({ content, icon, color = "white", background, onClick, radius = "rounded-md", disabled = false }) {
+function Button({ preset, content, startIcon, endIcon, color = "white", background, onClick, radius = "rounded-md", disabled = false }) {
+	let history = useHistory();
+
+	if (preset === "back") {
+		return (
+			<button className="flex items-center justify-center py-3 px-4" onClick={() => history.goBack()}>
+				<img src={iconsDark.backIcon} alt="back button" />
+				<p className="text-black font-medium mr-4"> Back </p>
+			</button>
+		);
+	}
+
+	if (preset === "main") {
+		return (
+			<button
+				onClick={onClick}
+				disabled={disabled}
+				className="bg-green flex items-center justify-center py-3 px-5 gap-2 rounded-md hover:bg-green50 transition-all duration-300">
+				{startIcon ? <img src={startIcon} alt="icon" /> : ""}
+				{content ? <p className={`text-white font-medium mr-4`}> {content} </p> : ""}
+				{endIcon ? <img src={endIcon} alt="icon" /> : ""}
+			</button>
+		);
+	}
+
+	if (preset === "secondary") {
+		return (
+			<button
+				onClick={onClick}
+				disabled={disabled}
+				className="flex items-center justify-center py-3 px-5 gap-2 rounded-md border-black70 border-2 hover:bg-green50 transition-all duration-300">
+				{content ? <p className={`text-black70 font-medium mr-4`}> {content} </p> : ""}
+			</button>
+		);
+	}
+
 	return (
 		<button
 			onClick={onClick}
 			disabled={disabled}
-			className={`bg-${background} flex items-center justify-center py-4 px-6 ${radius} hover:bg-${background}50 transition-all duration-300`}>
+			className={`bg-${background} flex items-center justify-center py-3 px-5 gap-2 ${radius} hover:bg-${background}50 transition-all duration-300`}>
+			{startIcon ? <img src={startIcon} alt="icon" /> : ""}
 			{content ? <p className={`text-${color} font-medium mr-4`}> {content} </p> : ""}
-			<img src={icon} alt="icon" />
+			{endIcon ? <img src={endIcon} alt="icon" /> : ""}
 		</button>
 	);
 }
